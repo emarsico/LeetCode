534ef16f0719f73ef249071421c7a98503b24e95
elliotmarsico <elliotmarsico@outlook.com>
2024-01-09 17:38:27 -0500

    1/9/24

---
+++ ReverseList.cs
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        if(head == null || head.next == null)
+        {
+            return head;
+        }
+
+        ListNode prevNode = head; // 1
+        ListNode interNode = head.next; // 2
+        head.next = null;
+        ListNode futureNode = null;
+        if(interNode.next != null)
+        {
+            futureNode = interNode.next; // 3
+        }
+
+        while(futureNode != null)
+        {
+            
+            interNode.next = prevNode; // 2 -> 1
+            prevNode = interNode; // 2
+            interNode = futureNode;
+            futureNode = futureNode.next;
+
+        }
+
+        interNode.next = prevNode;
+
+        return interNode;
+    }
+}
+
+// [1, 2, 3]
+/*
+    1 -> 2 -> 3 -> 4    1 2 3
+    1 <- 2 // 3 -> 4    2 3 4
+    1 <- 2 <- 3 // 4    3 4 null
+
+*/
