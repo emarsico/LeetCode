edcd3929bd4a92bf94970b04beae6fb555677845
Elliot Marsico <70075322+emarsico@users.noreply.github.com>
2024-01-21 19:17:26 -0500

    Daily Submission

---
+++ RecentCounter.cs
@@ -0,0 +1,27 @@
+
+ // O(n) Computational Complexity
+// 1/21/24
+
+public class RecentCounter {
+    List<int> counter;
+
+    public RecentCounter() {
+        counter = new List<int>();
+    }
+    
+    public int Ping(int t) {
+        counter.Add(t);
+
+        while(counter[0] < (t-3000)){
+            counter.RemoveAt(0);
+        }
+
+        return counter.Count;
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.Ping(t);
+ */
